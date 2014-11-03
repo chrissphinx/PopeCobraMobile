e414457fc351c04ee70deeec8eea6369ca366fb2
chrissphinx <chrissphinx@gmail.com>
2014-11-03 18:55:53 -0500

    testing double api callback

--- app/src/main/java/com/popecobra/mobile/APITest.java
+++ app/src/main/java/com/popecobra/mobile/APITest.java
@@ -7,7 +7,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -21,6 +29,7 @@ import retrofit.http.Path;
 
 public class APITest extends Activity {
     private static final String TAG = "APITest";
+    private static final int OBJECT_ID = 73;
     @InjectView(R.id.response) TextView mResponse;
 
     @Override
@@ -29,7 +38,7 @@ public class APITest extends Activity {
         setContentView(R.layout.activity_apitest);
         ButterKnife.inject(this);
 
-        API api = new RestAdapter.Builder()
+        final API api = new RestAdapter.Builder()
             .setEndpoint("http://api.popecobra.com/1.0")
             .setRequestInterceptor(new RequestInterceptor() {
                 @Override
@@ -40,15 +49,37 @@ public class APITest extends Activity {
             .build()
             .create(API.class);
 
-        api.getRecords(126, new Callback<JsonObject>() {
+        api.getFields(OBJECT_ID, new Callback<JsonObject>() {
             @Override
-            public void success(JsonObject jsonObject, Response response) {
-                mResponse.setText(jsonObject.toString());
+            public void success(JsonObject object, Response response) {
+                final JsonArray fields = object.getAsJsonArray("fields");
+
+                api.getRecords(OBJECT_ID, new Callback<JsonObject>() {
+                    @Override
+                    public void success(JsonObject object, Response response) {
+                        Type listType = new TypeToken<ArrayList<Map<String, String>>>() {}.getType();
+                        ArrayList<Map<String, String>> list = new Gson().fromJson(object.get("values"), listType);
+                        for (Map<String, String> item : list) {
+                            for (JsonElement field : fields) {
+                                String name = field.getAsJsonObject().get("name").getAsString();
+                                Log.d(TAG, name + ": " + item.get(name));
+                            }
+                            Log.d(TAG, "--------------------");
+                        }
+
+                        mResponse.setText(object.toString());
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        Log.e(TAG, error.getMessage());
+                    }
+                });
             }
 
             @Override
             public void failure(RetrofitError error) {
-                Log.e(TAG, error.toString());
+                Log.e(TAG, error.getMessage());
             }
         });
     }
@@ -68,5 +99,8 @@ public class APITest extends Activity {
     private interface API {
         @GET("/objects/{id}/records")
         void getRecords(@Path("id") int id, Callback<JsonObject> callback);
+
+        @GET("/objects/{id}/fields")
+        void getFields(@Path("id") int id, Callback<JsonObject> callback);
     }
 }
